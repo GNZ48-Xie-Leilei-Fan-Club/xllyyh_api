7da7751d17ac97a6513f6dd632b45380074aa96d
Yuhuai Liu <adlius45@hotmail.com>
2019-03-08 03:46:52 -0500

    use django-i18n

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
+ 
 from api.models.keyworded_response import KeywordedResponse
 
 admin.site.register(KeywordedResponse)
+
+admin.site.index_title = _('所有管理项目')
+admin.site.site_header = _('谢蕾蕾应援会管理页面')
+admin.site.site_title = _('谢蕾蕾应援会管理页面')
--- api/models/keyworded_response.py
+++ api/models/keyworded_response.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class KeywordedResponse(models.Model):
-    keyword = models.TextField()
-    response = models.TextField()
+    keyword = models.TextField(verbose_name=_('关键词'))
+    response = models.TextField(verbose_name=_('应答'))
+
+    class Meta:
+        verbose_name = _('关键词应答')
+        verbose_name_plural = _('关键词应答')
 
     class JSONAPIMeta:
         resource_name = 'keyworded_responses'
