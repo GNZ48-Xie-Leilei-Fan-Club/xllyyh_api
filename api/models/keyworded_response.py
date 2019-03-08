bc811d72ba3d6dad5c039adabcb37e8b9d71327d
Yuhuai Liu <adlius45@hotmail.com>
2019-03-08 04:09:47 -0500

    __str__ method to model

--- api/models/keyworded_response.py
+++ api/models/keyworded_response.py
@@ -5,6 +5,9 @@ class KeywordedResponse(models.Model):
     keyword = models.TextField(verbose_name=_('关键词'))
     response = models.TextField(verbose_name=_('应答'))
 
+    def __str__(self):
+        return self.keyword
+
     class Meta:
         verbose_name = _('关键词应答')
         verbose_name_plural = _('关键词应答')
