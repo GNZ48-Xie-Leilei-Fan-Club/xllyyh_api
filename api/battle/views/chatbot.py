033533a2571fb55cddc6a29dca89ed2f92e75634
Yuhuai Liu <adlius45@hotmail.com>
2019-05-29 01:10:49 -0400

    order by descending order

--- api/battle/views/chatbot.py
+++ api/battle/views/chatbot.py
@@ -8,7 +8,7 @@ from api.models.battle import BattleCampaign
 class IndividualBattleBroadcastView(View):
 
     def get(self, request):
-        all_campaigns = BattleCampaign.objects.all().order_by('amount')
+        all_campaigns = BattleCampaign.objects.all().order_by('-amount')
         response_text = '当前PK战况\n'
         for campain in all_campaigns:
             response_text += '{}: {}\n'.format(campain.member_name, campain.amount)
