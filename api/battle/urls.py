797f7685a17b7e5c3448c518f38fe75f676b23ce
Yuhuai Liu <adlius45@hotmail.com>
2019-05-29 00:21:56 -0400

    add new endpoint for battle broadcast

--- api/battle/urls.py
+++ api/battle/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from api.battle.views.datav import IndividualView, GroupView
+from api.battle.views.chatbot import IndividualBattleBroadcastView
 
 urlpatterns = [
     path('individual/', IndividualView.as_view()),
     path('group/', GroupView.as_view()),
+    path('battle_broadcast/', IndividualBattleBroadcastView.as_view()),
 ]
---
+++ api/battle/views/chatbot.py
@@ -0,0 +1,18 @@
+import json
+from django.http import HttpResponse
+from django.views import View
+
+from api.models.battle import BattleCampaign
+
+
+class IndividualBattleBroadcastView(View):
+
+    def get(self, request):
+        all_campaigns = BattleCampaign.objects.all().order_by('amount')
+        response_text = '当前PK战况\n'
+        for campain in campaigns:
+            response_text += '{}: {}\n'.format(campain.member_name, campain.amount)
+        return HttpResponse(json.dumps({
+            'response': response_text,
+        }))
+
