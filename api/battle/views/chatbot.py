566380270c8b99632a7416912d49f86820c8982c
Yuhuai Liu <adlius45@hotmail.com>
2019-05-29 00:31:54 -0400

    minor fix

--- api/battle/views/chatbot.py
+++ api/battle/views/chatbot.py
@@ -10,7 +10,7 @@ class IndividualBattleBroadcastView(View):
     def get(self, request):
         all_campaigns = BattleCampaign.objects.all().order_by('amount')
         response_text = '当前PK战况\n'
-        for campain in campaigns:
+        for campain in all_campaigns:
             response_text += '{}: {}\n'.format(campain.member_name, campain.amount)
         return HttpResponse(json.dumps({
             'response': response_text,
