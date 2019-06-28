8b29dfd32dc5b0402c5287ccc73d27a44241bc3d
Yuhuai Liu <adlius45@hotmail.com>
2019-06-28 01:21:26 -0400

    add url

--- api/battle/urls.py
+++ api/battle/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from api.battle.views.datav import IndividualView, GroupView
+from api.battle.views.datav import IndividualView, GroupView, NotificationView
 from api.battle.views.chatbot import IndividualBattleBroadcastView
 
 urlpatterns = [
     path('individual/', IndividualView.as_view()),
     path('group/', GroupView.as_view()),
     path('battle_broadcast/', IndividualBattleBroadcastView.as_view()),
+    path('notification/', NotificationView.as_view()),
 ]
