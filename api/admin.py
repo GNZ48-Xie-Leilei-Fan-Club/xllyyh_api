7174f42a17a667b7cc1233a82f8a2503e2fd5bbe
Yuhuai Liu <adlius45@hotmail.com>
2019-06-28 01:19:02 -0400

    add BattleNotification to admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
  
-from api.models import KeywordedResponse, IgnoreNumber, CardSet, Card, Campaign, Rarity, FanClubUser, ModianUser, CardUserAssociation, Order, BattleCampaign
+from api.models import KeywordedResponse, IgnoreNumber, CardSet, Card, Campaign, Rarity, FanClubUser, ModianUser, CardUserAssociation, Order, BattleCampaign, BattleNotification
 
 admin.site.register(KeywordedResponse)
 admin.site.register(IgnoreNumber)
@@ -14,6 +14,7 @@ admin.site.register(ModianUser)
 admin.site.register(CardUserAssociation)
 admin.site.register(Order)
 admin.site.register(BattleCampaign)
+admin.site.register(BattleNotification)
 
 admin.site.index_title = _('所有管理项目')
 admin.site.site_header = _('谢蕾蕾应援会管理页面')
